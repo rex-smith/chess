a9bc1817b677b6f86e433e5d38bc45f235e39531
Rex Smith <rex.smith17@gmail.com>
2022-01-26 13:05:08 -0600

    Game ending methods added.

--- lib/game.rb
+++ lib/game.rb
@@ -12,17 +12,43 @@ class Game
       @board.displayBoard
       @board.active_player.move
     end
+    end_game
   end
 
   def game_over?
-
+    checkmate? || stalemate?
+  end
+  
+  def end_game
+    if checkmate?
+      puts "Checkmate! #{@board.active_player} wins the game!"
+    elsif stalemate?
+      puts "Stalemate!"
+    end
   end
 
   def checkmate?
-
+    # Conditions: In Check
+    # No possible moves
+    if @board.active_enemy.check? && @board.active_enemy.possible_moves.length == 0
+      return true
+    end
+    return false
   end
 
   def stalemate?
+    # # Stalemate piece combos
+    # # King vs. King
+    # # King Bishop vs. King
+    # # King Knight vs. King
+    # # King Bishop vs King Bishop (same bishop color)
+    # @board.active_player.pieces
+    # @board.active_enemy.pieces
 
+    # Or Not in Check and no possible moves
+    if !@board.active_player.check? && @board.active_player.possible_moves.length == 0
+      return true
+    end
+    return false
   end
 end
